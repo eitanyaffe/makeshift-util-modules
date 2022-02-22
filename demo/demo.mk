8461e69a6737ef5e14aef8a5015d40224b167492
Eitan Yaffe <eitan.yaffe@gmail.com>
2022-02-22 14:03:51 -0800

    demo module

---
+++ demo/demo.mk
@@ -0,0 +1,30 @@
+####################################################################################
+# rule for single subject
+####################################################################################
+
+DEMO_SUBJECT_DONE?=$(DEMO_SUBJECT_DIR)/.done
+$(DEMO_SUBJECT_DONE):
+	$(call _start,$(DEMO_SUBJECT_DIR))
+	echo "$(DEMO_SUBJECT_NAME) is $(DEMO_SUBJECT_HEIGHT)cm" > $(DEMO_SUBJECT_TABLE)
+	$(_end_touch)
+demo_subject: $(DEMO_SUBJECT_DONE)
+
+####################################################################################
+# rule for all subjects
+####################################################################################
+
+DEMO_ALL_DONE?=$(DEMO_INFO_DIR)/.done
+$(DEMO_ALL_DONE):
+	$(call _start,$(DEMO_INFO_DIR))
+	$(MAKE) m=par par_tasks_complex \
+		PAR_MODULE=demo \
+		PAR_NAME=demo_task \
+		PAR_WORK_DIR=$(DEMO_INFO_DIR) \
+		PAR_TARGET=demo_subject \
+		PAR_TASK_DIR=$(DEMO_SUBJECT_DIR) \
+		PAR_TASK_ITEM_TABLE=$(DEMO_SUBJECT_PARAM_TABLE) \
+		PAR_TASK_ITEM_VAR=DEMO_SID \
+		PAR_TASK_ODIR_VAR=DEMO_SUBJECT_DIR \
+		PAR_MAKEFLAGS="$(PAR_MAKEOVERRIDES)"
+	$(_end_touch)
+demo_subjects: $(DEMO_ALL_DONE)
---
+++ demo/demo_int.mk
@@ -0,0 +1,39 @@
+#####################################################################################################
+# register module
+#####################################################################################################
+
+units=demo.mk
+$(call _register_module,demo,$(units),)
+
+#####################################################################################################
+# module basic definitions
+#####################################################################################################
+
+# include module version in directory
+DEMO_VER?=v1.00
+DEMO_DIR?=$(OUTPUT_DIR)/demo/$(DEMO_VER)
+
+#####################################################################################################
+# demo.mk parameters
+#####################################################################################################
+
+# subject id
+DEMO_SID?=s1
+DEMO_SUBJECT_DIR?=$(DEMO_DIR)/subjects/$(DEMO_SID)
+
+# rule parameters
+DEMO_SUBJECT_NAME?=Jane
+DEMO_SUBJECT_HEIGHT?=160
+
+# output per subject
+DEMO_SUBJECT_TABLE?=$(DEMO_SUBJECT_DIR)/subject.txt
+
+#####################################################################################################
+# GCP directories
+#####################################################################################################
+
+# GCP work directory
+DEMO_INFO_DIR?=$(DEMO_DIR)/info
+
+# all run parametes are here
+DEMO_SUBJECT_PARAM_TABLE?=$(INPUT_DIR)/params.txt
